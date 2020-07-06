eb79aae4a798b6f346bc0397085054af158d94e6
Thieu Nguyen <thieu@bluegrassdigital.com>
2020-07-06 17:58:39 +0700

    Open New Screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './product/product.component';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'product',
+    component: ProductComponent,
   }
 ];
 @NgModule({
